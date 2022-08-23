0b017a3c53bb9823541fa318ef64105e3bed129d
jasuhot1@gmail.com <jah09>
2022-08-23 10:21:50 +0800

    ako ge push ang akong tubig solution file

---
+++ tubig/tubig/tubig/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tubig
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            // InitializeComponent();
+            //LoginPage = new NavigationPage(LoginPage);
+            //MainPage = new NavigationPage(new LoginPage());
+            //LoginPage();
+
+           
+
+        }
+
+       // public NavigationPage MainPage { get; }
+    }
+}
